7db87f70662fe65f0c25edf543a6ff40d0ae6107
Allan M Alegretti <allanalegretti@gmail.com>
2022-11-06 20:52:33 -0300

    cards sendo criados dinamicamente

--- main.js
+++ main.js
@@ -1,26 +1,35 @@
-function createCard(){
+function createGame(player1, hour, player2) {
+    return `
+    <li>
+        <img src="./assets/icon-${player1}.svg" alt="Bandeira do ${player1}">
+        <strong>${hour}</strong>
+        <img src="./assets/icon-${player2}.svg" alt="Bandeira da ${player2}">
+    </li>
+    `
+}
+
+function createCard(date, day, games){
     return `
     <div class="card">
-        <h2>24/11 <span>quinta</span></h2>
+        <h2>${date} <span>${day}</span></h2>
         <ul>
-            <li>
-                <img src="./assets/icon-brazil.svg" alt="Bandeira do Brasil">
-                <strong>16:00</strong>
-                <img src="./assets/icon-serbia.svg" alt="Bandeira da Sérvia">
-            </li>
+        ${games};
         </ul>
     </div>
     `
 }
-
-
 
 document.querySelector('#app').innerHTML = `
 <header>
     <img src="./assets/logo.svg" alt="Logo">
 </header>
 <main id="cards">
-    ${createCard()}
-    ${createCard()}
+    ${createCard('24/11', 'quinta', createGame('brazil', '16:00', 'serbia'))}
+
+    ${createCard('28/11', 'segunda',
+    createGame('brazil', '13:00', 'switzerland')+
+    createGame('portugal', '16:00', 'uruguai'))}
+
+    ${createCard('02/12', 'sexta', createGame('brazil', '16:00', 'cameroon'))}
 </main>
 `
